ffb252f82f83eb2ddbf5ba4cfdfdb6f748a70fa4
Donatas A <anushauskas@gmail.com>
2026-09-23 11:18:10 +0000

    Java setup

---
+++ java/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		int result = add(5, 3);
+		System.out.println("Hello, World! Result: " + result);
+	}
+
+	private static int add(int a, int b) {
+		return a + b;
+	}
+
+}
